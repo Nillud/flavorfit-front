6573a62cfb04ef2f32ddc7f77f02e34ab22d1d00
Nillud <agadullin.linar@mail.ru>
2026-03-01 13:08:19 +0500

    middleware

--- src/shared/constants/app.constants.ts
+++ src/shared/constants/app.constants.ts
@@ -1,2 +1,5 @@
 export const IS_CLIENT = typeof window !== "undefined";
 export const SITE_NAME = 'FlavorFit'
+
+export const ACCESS_TOKEN = 'accessToken'
+export const REFRESH_TOKEN = 'refreshToken'
---
+++ src/shared/lib/server/jwt-verify.server.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import { jwtVerify } from 'jose'
+
+import type { Role } from '@/__generated__/graphql'
+
+type TAuthTokenData = {
+  id: string
+  role: Role
+}
+
+export async function jwtVerifyServer(accessToken: string) {
+  try {
+    const { payload }: { payload: TAuthTokenData } = await jwtVerify(
+      accessToken,
+      new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+    )
+    return payload
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      error.message.includes('exp claim timestamp check failed')
+    ) {
+      console.log('Token has expired')
+      return null
+    }
+
+    console.log('Error verifying JWT:', error)
+    return null
+  }
+}
